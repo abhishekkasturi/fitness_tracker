076ddd8e1987d77f412c51d02e24eb34dfe322e4
T.R.Abhishek <amazingabhi.pgt@gmail.com>
2026-03-13 19:59:33 +0530

    corrected syntax error in dev_auth.py

--- dev_auth.py
+++ dev_auth.py
@@ -1,1 +1,15 @@
-import
+import os
+
+# Admin password stored in environment variables
+ADMIN_PASSWORD = os.getenv("ADMIN_PASSWORD")
+
+
+def verify_admin(password: str):
+    """
+    Verify developer/admin login
+    """
+
+    if password == ADMIN_PASSWORD:
+        return True
+
+    return False
