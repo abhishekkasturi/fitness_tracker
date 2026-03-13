8f223a46a3292529d75a88ae311a8200bf6575b5
T.R.Abhishek <amazingabhi.pgt@gmail.com>
2026-03-13 19:29:27 +0530

    fixed main.py syntax error

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-import
+from fastapi import FastAPI, Request, Form
+from fastapi.responses import HTMLResponse, RedirectResponse
+from fastapi.templating import Jinja2Templates
+from starlette.middleware.sessions import SessionMiddleware
+
+import os
+import database
+import auth
+import dev_auth
+
+app = FastAPI()
+
+app.add_middleware(
+    SessionMiddleware,
+    secret_key=os.getenv("SESSION_SECRET")
+)
+
+templates = Jinja2Templates(directory="templates")
+
+database.init_db()
