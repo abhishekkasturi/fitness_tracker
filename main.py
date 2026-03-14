9b3981d66c1757ed295fc31216a27c8c50818fcf
T.R.Abhishek <amazingabhi.pgt@gmail.com>
2026-03-14 08:17:12 +0530

    added landing page

--- main.py
+++ main.py
@@ -11,12 +11,20 @@ import dev_auth
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
 
 app.add_middleware(
     SessionMiddleware,
     secret_key=os.getenv("SESSION_SECRET")
 )
 
-templates = Jinja2Templates(directory="templates")
+# templates = Jinja2Templates(directory="templates")
 
 database.init_db()
+
+@app.get("/", response_class=HTMLResponse)
+def landing(request: Request):
+    return templates.TemplateResponse(
+        "landing.html",
+        {"request": request}
+    )
